ceb76577ebeb97815cd5973a131b924aa456f6e7
Lukas Koeller <acc.koeller@protonmail.com>
2024-07-24 13:05:01 +0200

    chore: setting console logs to new and old

--- src/News.tsx
+++ src/News.tsx
@@ -1,35 +1,46 @@
 import { useNavigate } from "@tanstack/react-router";
-import { FC, useEffect, useState } from "react";
-import { newsRoute } from "./router";
+import { FC, useEffect } from "react";
 
 export const News: FC = () => {
-  const navigate = useNavigate({ from: newsRoute.fullPath });
-  const { newsId } = newsRoute.useSearch();
-  const [news, setNews] = useState<Record<string, unknown>>();
-
-  setTimeout(() => {
-    setNews({
-        title: "News Title",
-        content: "News Content",
-        id: 1,
-    });
-  }, 1000);
+  const navigate = useNavigate({ from: "/news" });
 
   useEffect(() => {
-    if (!newsId && news?.id) {
-      console.log("useEffect after if:true");
-      void navigate({
-        search: (old) => ({
+    void navigate({
+      search: (old) => {
+        console.log("%cnavigate:firstId", "color: green", {
+          old,
+          new: {
+            ...old,
+            firstId: 1337,
+          },
+        });
+
+        return {
           ...old,
-          newsId: news.id,
-          title: news.title,
-          content: news.content,
-        }),
-        params: true,
-        replace: true,
-      });
-    }
-  }, [navigate, news?.id, newsId]);
+          firstId: 1337,
+        };
+      },
+    });
+  }, [navigate]);
+
+  useEffect(() => {
+    void navigate({
+      search: (old) => {
+        console.log("%cnavigate:secondId", "color: red", {
+          old,
+          new: {
+            ...old,
+            secondId: 42,
+          },
+        });
+
+        return {
+          ...old,
+          secondId: 42,
+        };
+      },
+    });
+  }, [navigate]);
 
   return (
     <div>
